758bfa8aff9d6dcb33c3f16b1b843d963067c1af
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-12 20:17:59 +0000

    finished the project, just creating a simple connection to the database

--- src/MyJDBC.java
+++ src/MyJDBC.java
@@ -1,5 +1,16 @@
+import java.sql.*;
+
 public class MyJDBC {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws SQLException {
+
+        // Form a connection to the database
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/new_schema", "root", "Twins2012");
+        Statement statement = connection.createStatement();
+        ResultSet set = statement.executeQuery("select * from people");
+
+        while(set.next()){
+            System.out.println(set.getString("firstName"));
+        }
+
     }
 }
